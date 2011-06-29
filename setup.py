cc159f8af4539b734529ece68df6934e4274ed9a
Adam Faulconbridge <afaulconbridge@googlemail.com>
2011-06-29 13:41:13 +0100

    Added setuptools

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name="RBNWorld", 
+    version="0.1.0", 
+    author="Adam Faulconbridge", 
+    author_email="afaulconbridge@googlemail.com", 
+#    packages=["AChemKit", "AChemKit/utils", "AChemKit/tools"],
+    packages = find_packages(),
+    classifiers = ["Development Status :: 3 - Alpha", 
+        "Intended Audience :: Science/Research", 
+        "License :: OSI Approved :: BSD License", 
+        "Natural Language :: English", 
+        "Operating System :: OS Independent", 
+        "Programming Language :: Python", 
+        "Programming Language :: Python :: 2.6", 
+        "Programming Language :: Python :: 2.7", 
+        "Topic :: Scientific/Engineering :: Artificial Life", 
+        "Topic :: Scientific/Engineering :: Chemistry", 
+        "Topic :: Software Development :: Libraries :: Python Modules"], 
+    url="https://github.com/afaulconbridge/RBNWorld", 
+    description="An Artificial Chemistry", 
+    long_description=open("README.txt").read() )
